5ad221cbdef219f073dc63b7899bba0a25d09074
alihakimov919@gmail.com <alihakimov919@gmail.com>
2023-08-01 20:07:17 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const navEl = document.querySelector('.nav')
+
+fetch('https://jsonplaceholder.typicode.com/photos').then((response) => {
+    return response.json()
+}).then((info) => {
+    console.log(info);
+    console.log(navEl);
+    info.map((el) => {
+        if(el.id <= 100){
+            const lol = document.createElement("div");
+            lol.classList.add('lol');
+            lol.innerHTML += `
+            <img class = "img" src = "${el["url"]}" alt = "">
+            <p><h2>${el["title"]}</h2></p>`;
+            navEl.appendChild(lol);
+
+            const img = lol.querySelector(".img");
+            img.addEventListener("click", () => {
+                if (img.style.position) {
+                    img.style.position = "";
+                    img.style.top = "";
+                    img.style.left = "";
+                    img.style.width = "";
+                    img.style.height = "";
+                } else {
+                    img.style.position = "absolute";
+                    img.style.position = "fixed";
+                    img.style.top = "100px";
+                    img.style.left = "500px";
+                    img.style.width = "500px";
+                    img.style.height = "500px";
+                }
+            })
+
+        }
+        
+    })
+}).catch((err) => {
+    console.log(err);
+})
